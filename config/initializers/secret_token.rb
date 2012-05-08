7a7b7899564b83d0867549bf0fd98746a2b54002
cristian <cristian.ivanoff@gmail.com>
2012-05-08 21:44:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlannerApp::Application.config.secret_token = '37dc0559a95dd7ea5e075c1e825989b56811fb62feeac4662826f530f200d099382e7c36eef105b6612c5374b6ff86717740b59347ffd489787f89d603dc715f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PlannerApp::Application.config.session_store :cookie_store, :key => '_planner_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PlannerApp::Application.config.session_store :active_record_store
